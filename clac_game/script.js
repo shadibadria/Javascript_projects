4a99e556cde07753c87036c44241a8b6a5ec456e
shadibadria <shadibadria@gmail.com>
2021-11-14 12:56:04 +0200

    calc game

--- clac_game/script.js
+++ clac_game/script.js
@@ -60,3 +60,37 @@ document.querySelector('.again').addEventListener('click', function(){
     biggest_number_rest()
     document.querySelector('body').style.backgroundColor = "#222"
 })
+
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+let modal_text = document.querySelector('.modal_text')
+const openModal = function () {
+  modal_text.textContent = `${number1} + ${number2}  = ${number1 + number2}`
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+  
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+// a way to add event listener 
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener('click', openModal);
+
+
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  console.log(e.key);
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
